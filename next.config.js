b65b2e7e2a7dd6f7b2c759256d0bd153f240b136
SG <research@alethic.ai>
2024-12-01 20:38:06 -0800

    img 3

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
-        protocol: "https",
-        hostname: "drive.google.com",
-        protocol: "https",
-        hostname: "eventservices.berkeley.edu",
-        protocol: "https",
-        hostname: "images.squarespace-cdn.com",
+        hostname: "cdn.theatlantic.com"
       },
     ],
   },
